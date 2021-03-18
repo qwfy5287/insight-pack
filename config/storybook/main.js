e1528eca79c3367b16aa760f7e5e66d89219f5a1
qwfy5287 <qwfy5287@qq.com>
2021-03-18 11:17:45 +0800

    feat: storybook + tailwindcss

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,4 +1,22 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [require('tailwindcss'), require('autoprefixer')],
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  },
 }
--- src/stories/vue.common.js
+++ src/stories/vue.common.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
+import '../assets/tailwind.css'
+
 /**
  * VueCompositionApi
  */
