376e5c66e920a5a0f39e40ce2d05a32328a15f93
qwfy5287 <qwfy5287@qq.com>
2021-03-24 16:34:42 +0800

    feat: --target lib 方式 temp01

--- packages/index.js
+++ packages/index.js
@@ -1,1 +1,20 @@
 import '../src/common/tree.common'
+
+import InsPager from './ins-pager'
+
+const components = [InsPager]
+
+const install = function(Vue) {
+  components.forEach(component => {
+    Vue.component(component.name, component)
+  })
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install,
+  InsPager,
+}
---
+++ packages/ins-pager/index.js
@@ -0,0 +1,7 @@
+import InsPager from '../../src/components/ins-capi/InsPager.vue'
+
+InsPager.install = function(Vue) {
+  Vue.component(InsPager.name, InsPager)
+}
+
+export default InsPager
