4ae7e12936e51a1dbad9903956e03a9743be1190
qwfy5287 <qwfy5287@qq.com>
2021-05-28 11:19:55 +0800

    feat: getChildrenRoutes

--- src/common/router.common.js
+++ src/common/router.common.js
@@ -1,3 +1,5 @@
+import { treeToListForRouter, listToTree } from './tree.common'
+
 /**
  * 通过 item.path 获取完整路径
  */
@@ -6,5 +8,21 @@ export const getFullPath = (basePath, routePath, $router) => {
   if (routePath) {
     result = $router.matcher.getRoutes().find((d) => d.path.endsWith(routePath))?.path
   }
+  return result
+}
+
+/**
+ * 获取子路由
+ * @param {Array} routes 全部路由 this.$router.options.routes
+ * @param {String} path 当前路由地址 this.$route.path
+ * @returns
+ */
+export const getChildrenRoutes = (routes, path) => {
+  let result = null
+  let list = treeToListForRouter(routes, 'path', 'name')
+  list?.forEach((d) => (d.path = d.fullPath))
+  let tree = listToTree(list)
+  let firstLevelItem = tree.find((d) => d.fullPath.startsWith(`/${path.split('/')[1]}`))
+  result = firstLevelItem?.children
   return result
 }
